1b935ec8601f5a4154b9c36d8d955b107f975177
EhsanSiavashi <siavashi.e@gmail.com>
2016-02-28 16:27:14 -0600

    Added files via upload

---
+++ run_analysis.R
@@ -0,0 +1,56 @@
+
+
+library("data.table")
+library("reshape2")
+
+# We assume that the downloaded file is in the working directory.
+
+Activity_types <- read.table("./UCI HAR Dataset/Activity_types.txt")[,2]
+
+
+features <- read.table("./UCI HAR Dataset/features.txt")[,2]
+
+
+extract_features <- grepl("mean|std", features)
+
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+
+names(X_test) = features
+
+X_test = X_test[,extract_features]
+
+
+y_test[,2] = Activity_types[y_test[,1]]
+names(y_test) = c("Activity", "Activity_type")
+names(subject_test) = "subject"
+
+test_data <- cbind(as.data.table(subject_test), y_test, X_test)
+
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+
+names(X_train) = features
+
+X_train = X_train[,extract_features]
+
+# Load activity data
+y_train[,2] = Activity_types[y_train[,1]]
+names(y_train) = c("Activity", "Activity_type")
+names(subject_train) = "subject"
+
+train_data <- cbind(as.data.table(subject_train), y_train, X_train)
+
+# Merge test and train tables
+data = rbind(test_data, train_data)
+
+id_labels   = c("subject", "Activity", "Activity_type")
+data_labels = setdiff(colnames(data), id_labels)
+melt_data      = melt(data, id = id_labels, measure.vars = data_labels)
+
+tidy_data   = dcast(melt_data, subject + Activity_type ~ variable, mean)
+
+write.table(tidy_data, file = "./tidy_data.txt")
